d554587d8aecf0f4e5d8639656151e86102366c6
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-06 09:14:42 +0000

    returns a map of groceries with the format (name, quantity)

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  // Create a new Map with the groceries items and properties
+  const groceryMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  return groceryMap;
+}
