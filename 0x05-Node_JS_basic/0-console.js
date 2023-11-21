5557885151d9dae13a4c02e523369ed0912d55fb
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-21 21:44:11 +0000

    used function instead of const

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,7 +2,8 @@
  * Function to display a message to the standard output
  * @param message (str): The message to display
  */
-const displayMessage(message) {
+
+function displayMessage(message) {
   console.log(message);
 }
 
