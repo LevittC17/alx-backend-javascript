d6abe0f69537bbaab496042999ee37649028f62a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-21 22:19:18 +0000

    removed documentation

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,3 @@
-/**
- * Displays a welcome message and prompts the user for their name
- * Accepts user input from stdin and prints the user's name
- * Displays a closing message when the program ends.
- */
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 // Listen for user input on stdin
@@ -20,6 +15,3 @@ process.stdin.on('end', () => {
   // Display closing message
   console.log("This important software is now closing");
 });
-
-// Set encoding to handle user input as strings
-process.stdin.setEncoding('utf-8');
