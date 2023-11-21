6cdd0dc5fa8b8d25465c5738cb674e8a8d8a704e
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-21 21:35:48 +0000

    added documentation

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,8 @@
-function displayMessage(message) {
+/**
+ * Function to display a message to the standard output
+ * @param message (str): The message to display
+ */
+const displayMessage(message) {
   console.log(message);
 }
 
