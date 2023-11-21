0592c077e57d6ed9f343606d1f62028151b5d76a
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-21 22:51:43 +0000

    removed documentations inside code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,18 @@
+/**
+ * Displays a welcome message and prompts the user for their name.
+ * Accepts user input from stdin and prints the user's name.
+ * Displays a closing message when the program ends.
+ */
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// Listen for user input on stdin
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if (name) {
-    // Display the user's name
     console.log(`Your name is: ${name}`);
   }
 });
 
-// Listen for the end of the program
 process.stdin.on('end', () => {
-  // Display closing message
   console.log("This important software is now closing");
 });
