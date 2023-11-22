68c33252392875b9f99078639f7a30b26a618fd0
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-22 13:06:00 +0000

    'Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// Create an HTTP server
+const app = http.createServer((req, res) => {
+  // Set the response headers
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the respose body
+  res.end('Hello Holberton School!\n');
+});
+
+// Listen on port 1245
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+
+// Export the app variable
+module.exports = app
