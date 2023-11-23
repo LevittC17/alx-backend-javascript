e7e41b637779dca1727c94ea9a5b1d4df82195b0
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-23 10:37:13 +0000

    running each test case individually

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,24 +1,28 @@
 const assert = require('assert');
+const { it, describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
 /**
  * Test suite for the calculateNumber function.
  */
-describe('calculateNumber', function () {
+describe('calculateNumber()', () => {
   /**
    * Test case for the calculateNumber function.
    * It should return the sum of rounded numbers.
    */
-  it('should return the sum of rounded numbers', function () {
+  it('should return the sum of rounded numbers', () => {
     // Test case 1
     assert.strictEqual(calculateNumber(1, 3), 4);
-
+  });
+  it('should return the sum of rounded numbers', () => {
     // Test case 2
     assert.strictEqual(calculateNumber(1, 3.7), 5);
-
+  });
+  it('should return the sum of rounded numbers', () => {
     // Test case 3
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-
+  });
+  it('should return the sum of rounded numbers', () => {
     // Test case 4
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
