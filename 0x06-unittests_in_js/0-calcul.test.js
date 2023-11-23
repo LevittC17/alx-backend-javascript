5e2414846534471d4dec377198332d2b1e38c175
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-23 10:47:24 +0000

    using ES6 syntax

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,25 +5,21 @@ const calculateNumber = require('./0-calcul');
 /**
  * Test suite for the calculateNumber function.
  */
-describe('calculateNumber()', () => {
+describe('calculateNumber', () => {
   /**
    * Test case for the calculateNumber function.
    * It should return the sum of rounded numbers.
    */
-  it('should return the sum of rounded numbers', () => {
-    // Test case 1
-    assert.strictEqual(calculateNumber(1, 3), 4);
+  it('Test case 1', () => {
+    assert.equal(calculateNumber(1, 3), 4);
   });
-  it('should return the sum of rounded numbers', () => {
-    // Test case 2
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  it('Test case 2', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('should return the sum of rounded numbers', () => {
-    // Test case 3
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('Test case 3', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return the sum of rounded numbers', () => {
-    // Test case 4
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  it('Test case 4', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
 });
