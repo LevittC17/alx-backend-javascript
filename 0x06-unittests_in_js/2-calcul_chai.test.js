c11954208eadfef1403e93818739448c0564017f
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-23 11:30:41 +0000

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', () => {
+  it('Perform SUM operation and round the result', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+  it('Perform SUBTRACT operation and round the result', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+  it('Perform DIVIDE operation, round the result', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+  it('Perform DIVIDE operation and handle division by 0', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+});
