30cdf30e6936fa7d57bf935c6877e8812ce77517
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-24 21:09:18 +0000

    ES6 syntax

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,26 @@
 const request = require("request");
-const {describe, it} = require("mocha");
-const expect = require("chai").expect;
+const { describe, it } = require("mocha");
+const { expect } = require("chai");
 
-describe("Index page", function() {
+describe("Index page", () => {
     const options = {
 	url: "http://localhost:7865/",
 	method: "GET"
     }
-    it("check correct status code", function(done) {
-	request(options, function(err, res, body) {
+    it("check correct status code", (done) => {
+	request(options, (err, res, body) => {
 	    expect(res.statusCode).to.equal(200);
 	    done();
 	});
     });
-    it("check correct content", function(done) {
-	request(options, function(err, res, body) {
+    it("check correct content", (done) => {
+	request(options, (err, res, body) => {
 	    expect(body).to.contain("Welcome to the payment system");
 	    done();
 	});
     });
-    it("check correct content length", function(done) {
-	request(options, function(err, res, body) {
+    it("check correct content length", (done) => {
+	request(options, (err, res, body) => {
 	    expect(res.headers['content-length']).to.equal('29');
 	    done();
 	});
