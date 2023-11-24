e9cdb117a5a2c629a9f236853bfdd81baedb7cd8
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-24 19:57:56 +0000

    redoing tests

--- unittests-0x06/6-payment_token.test.js
+++ unittests-0x06/6-payment_token.test.js
@@ -1,24 +1,16 @@
-const chai = require('chai');
-const expect = chai.expect;
+const { it, describe } = require('mocha');
+const { expect } = require('chai');
+const { getPaymentTokenFromAPI } = require('./6-payment_token');
 
-const getPaymentTokenFromAPI = require('./6-payment_token');
+describe('getPaymentTokenFromAPI', () {
+  it('should resolve with the correct response when success is true', (done) {
+    // Call the function with success set to true
+    getPaymentTokenFromAPI(true).then((result) => {
+      // Assert that the result is as expected
+      expect(result).to.deep.equal({ data: 'Successful response from the API' });
 
-describe('getPaymentTokenFromAPI', () => {
-  it('should return an instance of a Promise', () => {
-    const res = getPaymentTokenFromAPI();
-    expect(res).to.be.an.instanceof(Promise);
-  });
-  it("should return a JSON data object {data: 'Successful response from the API'", () => {
-    getPaymentTokenFromAPI(true)
-      .then((res) => {
-        expect(res.data).to.be.equal('Successful response from the API');
-        done();
-      });
-  });
-  it('should do nothing when not success', () => {
-    getPaymentTokenFromAPI(false)
-      .then((res) => {
-        expect(res).to.equal('');
-      });
+      // Call done to indicate that the async test is complete
+      done();
+    })
   });
 });
