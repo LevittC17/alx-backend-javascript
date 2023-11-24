021cfe10e62b1a9c833bdf1a2c481ca3a3eb3656
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-24 20:05:00 +0000

    redoing tests

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,27 +1,13 @@
-const { describe, it, done } = require('mocha');
+const { it, describe } = require('mocha');
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('Resolve with correct response when success = true', () => {
-    // Call the function with success set to true
-    getPaymentTokenFromAPI(true).then((result) => {
-      // Assert that the result is as expected
-      expect(result).to.deep.equal({ data: 'Successful response from the API' });
-
-      // Call done to indicate that the async test is complete
-      done();
-    });
-  });
-
-  it('Resolve with undefined when success = false', () => {
-    // Call the function with success set to false
-    getPaymentTokenFromAPI(false).then((result) => {
-      // Assert that the result is as expected
-      expect(result).to.equal(undefined);
-
-      // Call done to indicate that the async test is complete
-      done();
-    });
+  it('should resolve with the correct response when success is true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((data) => {
+        expect(data).to.have.property('data');
+        done();
+      });
   });
 });
