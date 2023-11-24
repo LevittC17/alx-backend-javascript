8715103ec7b4fe2a2da131f162cdc1a9831628a8
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-24 19:34:17 +0000

    Async test with done

--- unittests-0x06/6-payment_token.test.js
+++ unittests-0x06/6-payment_token.test.js
@@ -5,12 +5,10 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('Resolve with correct response when success = true', () => {
     // Call the function with success set to true
-    getPaymentTokenFromAPI(true).then((result) => {
-      // Assert that the result is as expected
-      expect(result).to.have.property('result');
-
-      // Call done to indicate that the async test is complete
-      done();
-    });
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.have.property('result');
+        done()  
+      });
   });
 });
