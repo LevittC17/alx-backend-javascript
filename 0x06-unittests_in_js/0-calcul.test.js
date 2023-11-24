d3e5705a44ae81fe7a9bf582d1ab4b148e0b9445
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-24 09:26:44 +0000

    correct output for test case 3

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,29 @@
 const assert = require('assert');
-const {it, describe} = require('mocha');
+const { it, describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-    it('checking if numbers round', () => {
-        assert.equal(calculateNumber(1, 3), 4);
-    });
-    it('checking if numbers round 2nd round', () => {
-        assert.equal(calculateNumber(1, 3.7), 5);
-    });
-    it('checking if numbers round 3nd round', () => {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
-    });
-    it('checking if numbers round 4nd round', () => {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
-    it('checking negative return', () => {
-        assert.equal(calculateNumber(-1.3, -3.7), -5);
-      });
+/**
+ * Test suite for the calculateNumber function.
+ */
+describe('calculateNumber()', () => {
+  /**
+   * Test case for the calculateNumber function.
+   * It should return the sum of rounded numbers.
+   */
+  it('should return the sum of rounded numbers', () => {
+    // Test case 1
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return the sum of rounded numbers', () => {
+    // Test case 2
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return the sum of rounded numbers', () => {
+    // Test case 3
+    assert.strictEqual(calculateNumber(1.2, 3.7), 6);
+  });
+  it('should return the sum of rounded numbers', () => {
+    // Test case 4
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
