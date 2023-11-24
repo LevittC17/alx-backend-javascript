5a1610c1c3f2d97b62537235b348afe9b97268b6
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-24 15:12:06 +0000

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,14 @@
+/**
+ * Get payment token from API
+ * @param {boolean} success - Indicates whether the API request is successful
+ * @returns {Promise<Object>} A promise with the APU response
+ */
+function getPaymentTokenFromAPI(success) {
+  if (success) {
+    return Promise.resolve({ data: 'Succesful response from the API' });
+  } else {
+    return Promise.resolve(undefined);
+  }
+}
+
+module.exports = getPaymentTokenFromAPI;
---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,27 @@
+const { describe, it, done } = require('mocha');
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('Resolve with correct response when success = true', () => {
+    // Call the function with success set to true
+    getPaymentTokenFromAPI(true).then((result) => {
+      // Assert that the result is as expected
+      expect(result).to.deep.equal({ data: 'Successful response from the API' });
+
+      // Call done to indicate that the async test is complete
+      done();
+    });
+  });
+
+  it('Resolve with undefined when success = false', () => {
+    // Call the function with success set to false
+    getPaymentTokenFromAPI(false).then((result) => {
+      // Assert that the result is as expected
+      expect(result).to.equal(undefined);
+
+      // Call done to indicate that the async test is complete
+      done();
+    });
+  });
+});
