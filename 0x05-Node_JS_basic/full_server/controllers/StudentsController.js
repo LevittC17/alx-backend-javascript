0312b791d50e46d3d6a0299df74804c7c586f994
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-25 02:20:50 +0000

    commonjs

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,37 +1,61 @@
 const readDatabase = require('../utils');
 
-export default class StudentsController {
-  static async getAllStudents(request, response) {
-    try {
-      const students = await readDatabase(process.argv[2].toString());
-      const output = ['This is the list of our students'];
+/**
+ * Controller class for handling student-related operations.
+ */
+class StudentsController {
+  /**
+   * Handles the request to get a list of all students grouped by fields.
+   *
+   * @param {Object} request - The Express request object.
+   * @param {Object} response - The Express response object.
+   */
+  static getAllStudents(request, response) {
+    // Read the student database and process the data
+    readDatabase(process.argv[2].toString()).then((students) => {
+      const output = [];
 
-      Object.keys(students).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' })).forEach((key) => {
-        const count = students[key].length;
-        const list = students[key].join(', ');
-        output.push(`Number of students in ${key}: ${count}. List: ${list}`);
-      });
+      // Construct the output array with information about each field
+      output.push('This is the list of our students');
+      const keys = Object.keys(students);
+      keys.sort();
+      for (let i = 0; i < keys.length; i += 1) {
+        output.push(`Number of students in ${keys[i]}: ${students[keys[i]].length}. List: ${students[keys[i]].join(', ')}`);
+      }
 
+      // Send the constructed output as the response
       response.status(200).send(output.join('\n'));
-    } catch (err) {
+    }).catch(() => {
+      // Handle errors when reading the database
       response.status(500).send('Cannot load the database');
-    }
+    });
   }
 
-  static async getAllStudentsByMajor(request, response) {
-    const { major } = request.params;
+  /**
+   * Handles the request to get a list of students in a specific major (field).
+   *
+   * @param {Object} request - The Express request object.
+   * @param {Object} response - The Express response object.
+   */
+  static getAllStudentsByMajor(request, response) {
+    // Extract the major parameter from the request
+    const field = request.params.major;
 
-    if (major !== 'CS' && major !== 'SWE') {
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
-    }
-
-    try {
-      const students = await readDatabase(process.argv[2].toString());
-      const list = students[major].join(', ');
-      response.status(200).send(`List: ${list}`);
-    } catch (err) {
+    // Read the student database and process the data
+    readDatabase(process.argv[2].toString()).then((students) => {
+      // Check if the specified major is valid
+      if (!(field in students)) {
+        response.status(500).send('Major parameter must be CS or SWE');
+      } else {
+        // Send the list of students in the specified major as the response
+        response.status(200).send(`List: ${students[field].join(', ')}`);
+      }
+    }).catch(() => {
+      // Handle errors when reading the database
       response.status(500).send('Cannot load the database');
-    }
+    });
   }
 }
+
+// Export the StudentsController class for use in other modules
+module.exports = StudentsController;
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,19 +1,48 @@
-import { readFile } from 'fs';
+const { readFile } = require('fs');
 
 /**
- * Reads the database asynchronously and returns a promise.
- * @param {string} filePath - The path to the CSV file.
- * @returns {Promise<object>} A promise that resolves with an object of
- * arrays of student first names per fields.
+ * Reads the student database file asynchronously and returns a promise.
+ *
+ * @param {string} filePath - The path to the student database file.
+ * @returns {Promise<Object>} - A promise that resolves to an object containing
+ * arrays of first names per field.
  */
-export default function readDatabase(filePath) {
+module.exports = function readDatabase(filePath) {
+  // Create an object to store students grouped by fields
+  const students = {};
+
   return new Promise((resolve, reject) => {
-    readFile(filePath, 'utf-8', (err, data) => {
+    // Read the content of the file
+    readFile(filePath, (err, data) => {
       if (err) {
+        // Reject the promise if there's an error reading the file
         reject(err);
       } else {
-        resolve(data);
+        // Convert the file content to an array of lines
+        const lines = data.toString().split('\n');
+        // Exclude the header line
+        const noHeader = lines.slice(1);
+
+        // Process each line of the file
+        for (let i = 0; i < noHeader.length; i += 1) {
+          if (noHeader[i]) {
+            // Split the line into fields
+            const field = noHeader[i].toString().split(',');
+
+            // Check if the field exists in the students object
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              // If it exists, push the current student's first name to the array
+              students[field[3]].push(field[0]);
+            } else {
+              // If it doesn't exist, create a new array with the current student's first name
+              students[field[3]] = [field[0]];
+            }
+          }
+        }
+
+        // Resolve the promise with the object containing students grouped by fields
+        resolve(students);
       }
     });
   });
-}
+};
