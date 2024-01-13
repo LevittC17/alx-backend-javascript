a3d4022eeba68309d2662230761ccefded96673d
LevittC17 <LeewednesdayC17@protonmail.com>
2024-01-13 15:26:13 +0300

    Printing Teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,6 +13,10 @@ interface Directors extends Teachers {
   numberOfReports: number;
 }
 
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
 // Example usage
 const teacher3: Teacher = {
   firstName: 'John',
@@ -34,3 +38,14 @@ const director1: Directors = {
 console.log(teacher3);
 
 console.log(director1);
+
+// Function to print teacher information
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstLetter = firstName.charAt(0).toUpperCase();
+  const formattedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  return `${firstLetter}. ${formattedLastName}`;
+};
+
+// Example usage
+const printedTeacher = printTeacher("John", "Doe");
+console.log(printedTeacher);
