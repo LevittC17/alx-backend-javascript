5378d1ef9091a9a942129899d39a0119d2d7ee9b
LevittC17 <LeewednesdayC17@protonmail.com>
2024-01-13 15:18:26 +0300

    building a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// task_1/js/main.ts
+
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; // Allow any additional attribute
+}
+
+// Example usage
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
